5630957660396a44c19de95b1e6598159aafba96
Tomasz Szczepanek <tomasz.szczepanek10@gmail.com>
2021-05-07 19:17:09 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+{
+    const tasks = [
+        {
+            content: "Nagrać lekcję",
+            done: false,
+        },
+        {
+            content: "Zjeść pierogi",
+            done: true,
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+            ${task.content}
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+    };
+
+    init();
+}
